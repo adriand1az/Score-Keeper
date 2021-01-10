5905a459a868153fc8bdf2750346f5b9f481eef7
Adrian <63692318+adriand1az@users.noreply.github.com>
2021-01-09 19:17:27 -0600

    Refactoring
    Refactor my code so its easier to understand

--- main.js
+++ main.js
@@ -1,48 +1,58 @@
-// Selcting Id's
-const player1 = document.querySelector('#player-1');
-const player2 = document.querySelector('#player-2')
+// Creating dictionaries for each button
+const p1 = {
+    score: 0,
+    button: document.querySelector('#player-1'),
+    display: document.querySelector('#player-1-score')
+}
+
+const p2 = {
+    score: 0,
+    button: document.querySelector('#player-2'),
+    display: document.querySelector('#player-2-score')
+}
+
+// Setting the reset button
 const reset = document.querySelector('#reset')
-const player1ScoreUpdate = document.querySelector('#player-1-score');
-const player2ScoreUpdate = document.querySelector('#player-2-score');
 const setScore = document.querySelector('#set-score');
 
-let player1Score = 0;
-let player2Score = 0;
+
+// Setting default values
 let winningScore = 3;
 let isGameOver = false;
+
+// Function update score
+function updateScore(player) {
+    if (!isGameOver) {
+        player.score++;
+
+        if (player.score === winningScore) {
+            isGameOver = true;
+        }
+        player.display.textContent = player.score;
+    }
+}
+
+// Player 1
+p1.button.addEventListener('click', function () {
+    updateScore(p1)
+})
+
+// Player 2
+p2.button.addEventListener('click', function () {
+    updateScore(p2)
+})
+
 
 // Restart function
 function restart() {
     isGameOver = false;
-    player1Score = 0;
-    player2Score = 0;
-    player1ScoreUpdate.textContent = 0;
-    player2ScoreUpdate.textContent = 0;
+    p1.score = 0;
+    p2.score = 0;
+    p1.display.textContent = 0;
+    p2.display.textContent = 0;
 }
 
-// Player 1
-player1.addEventListener('click', function () {
-    if (!isGameOver) {
-        player1Score++;
 
-        if (player1Score === winningScore) {
-            isGameOver = true;
-        }
-        player1ScoreUpdate.textContent = player1Score;
-    }
-
-})
-
-player2.addEventListener('click', function () {
-    if (!isGameOver) {
-        player2Score++;
-
-        if (player2Score === winningScore) {
-            isGameOver = true;
-        }
-        player2ScoreUpdate.textContent = player2Score;
-    }
-})
 
 // When changing select item
 setScore.addEventListener('change', function () {
@@ -51,5 +61,5 @@ setScore.addEventListener('change', function () {
 })
 
 // reset button
-reset.addEventListener('click', restart)
+reset.addEventListener('click', restart);
 
