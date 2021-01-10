08a3e6d6e47fcbf019c41e8b67628cf614feba47
Adrian <63692318+adriand1az@users.noreply.github.com>
2021-01-09 18:47:32 -0600

    Javascript
    Fully functioning. All thats missing now is the styling

--- main.js
+++ main.js
@@ -2,36 +2,54 @@
 const player1 = document.querySelector('#player-1');
 const player2 = document.querySelector('#player-2')
 const reset = document.querySelector('#reset')
+const player1ScoreUpdate = document.querySelector('#player-1-score');
+const player2ScoreUpdate = document.querySelector('#player-2-score');
+const setScore = document.querySelector('#set-score');
 
 let player1Score = 0;
 let player2Score = 0;
+let winningScore = 3;
+let isGameOver = false;
 
-
-// When clicked change color
-// Player 1
-player1.addEventListener('click', function () {
-    this.style.backgroundColor = ('green');
-    player2.style.backgroundColor = ('red');
-    player1Score++;
-    console.log(`Player 1 Score: ${player1Score}`);
-})
-
-// Player 2
-player2.addEventListener('click', function () {
-    this.style.backgroundColor = ('green');
-    player1.style.backgroundColor = ('red');
-    player2Score++;
-    console.log(`Player 2 Score: ${player2Score}`);
-})
-
-// Reset
-reset.addEventListener('click', function () {
-    player1.style.backgroundColor = '';
-    player2.style.backgroundColor = '';
-    this.style.backgroundColor = 'red';
+// Restart function
+function restart() {
+    isGameOver = false;
     player1Score = 0;
     player2Score = 0;
+    player1ScoreUpdate.textContent = 0;
+    player2ScoreUpdate.textContent = 0;
+}
+
+// Player 1
+player1.addEventListener('click', function () {
+    if (!isGameOver) {
+        player1Score++;
+
+        if (player1Score === winningScore) {
+            isGameOver = true;
+        }
+        player1ScoreUpdate.textContent = player1Score;
+    }
+
 })
 
-// Setting the option to choose the total score before a winner is declared
+player2.addEventListener('click', function () {
+    if (!isGameOver) {
+        player2Score++;
+
+        if (player2Score === winningScore) {
+            isGameOver = true;
+        }
+        player2ScoreUpdate.textContent = player2Score;
+    }
+})
+
+// When changing select item
+setScore.addEventListener('change', function () {
+    winningScore = parseInt(this.value)
+    restart()
+})
+
+// reset button
+reset.addEventListener('click', restart)
 
